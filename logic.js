8a01d3666d9dd6d8a2165e2251b30c2631848a10
ShrutiVNair <80637320+ShrutiVNair@users.noreply.github.com>
2023-05-11 22:55:20 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,4 @@
+// Sample data for books
+const books = [
+	{ title: 'Book 1', author: 'Author 1', subject: 'Subject 1', publishDate: '2022-01-01' },
+	{ title: 'Book 2', author: 'Author 2', subject: 'Subject 2
